a8811b16df1e10a7fba53d9f919d1f2a39af0f3d
최하은 <121525026+haeun320@users.noreply.github.com>
2025-11-23 13:24:50 +0900

    feat: test button 추가

---
+++ ChatViewer/ChatViewer/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace ChatViewer.ViewModel
+{
+    public partial class MainViewModel : ObservableObject 
+    {
+        [ObservableProperty]
+        private string _title = "Test";
+
+        private ICommand _buttonCommand;
+        public ICommand ButtonCommand => _buttonCommand ??= new RelayCommand(ExecuteButtonCommand);
+        public MainViewModel()
+        {
+
+        }
+
+        private void ExecuteButtonCommand()
+        {
+            Title = "Button Clicked!";
+        }
+    }
+}
